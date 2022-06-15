53d68a8946736cbc5a35324b047f0a4e2f4406e8
nathanjh-28 <nathan@nathanjharris.com>
2022-06-15 15:11:20 -0500

    reset app to blank slate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+/*
+
+Figma Link
+https://www.figma.com/file/4ctPLUvIn5b5Ep6YPOZWWd/Digital-Business-Card?node-id=0%3A1
+
+Requirements
+- Build from scratch
+- Fill in your own information
+-  Separate components for:
+    - info (photo, name, buttons, etc)
+    - About
+    - Interests
+    - Footer (social icons)
+
+*/
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<h1>Digital Business Card Prjt</h1>, document.getElementById('root'));

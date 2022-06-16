80553aa4cd199593a894c28384f54be36759e12c
nathanjh-28 <nathan@nathanjharris.com>
2022-06-16 11:28:23 -0500

    large chunk of styling done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Buttons from './components/Buttons';
 import About from './components/About';
 import Interests from './components/Interests';
 import Footer from './components/Footer';
+import './style.css'
 
 function App() {
     return (
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,9 +5,11 @@ function Heading() {
     return (
         <header>
             <img src={pic} className="profile-pic"></img>
-            <h2>Lauren Smith</h2>
-            <h4>Frontend Developer</h4>
-            <p>laurensmith.website</p>
+            <div className="header-text">
+                <h2>Lauren Smith</h2>
+                <h4>Frontend Developer</h4>
+                <p>laurensmith.website</p>
+            </div>
         </header>
     )
 }
